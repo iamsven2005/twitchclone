a110403df946cff17276f7e99e8117ecb152e6d0
Sven <iamsven2005@gmail.com>
2024-01-03 22:14:12 +0800

    vercel 1 test

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,19 @@
+import { Logo } from "./_components/logo";
+const AuthLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className="hero min-h-screen"> 
+        <div className="hero-content text-center">
+        <div className="max-w-md ">   
+        <Logo/>    
+        {children}
+        </div>
+        </div>
+        </div> 
+     );
+}
+ 
+export default AuthLayout;
