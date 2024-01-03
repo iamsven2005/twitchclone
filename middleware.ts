83a8cf2dcb2d810d9c19a64f7101ff4e52dbebdd
Sven <iamsven2005@gmail.com>
2024-01-04 00:03:09 +0800

    maybe not vercel?

--- middleware.ts
+++ middleware.ts
@@ -7,5 +7,11 @@ export default authMiddleware({});
  
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  runtime: 'experimental-edge',
+  unstable_allowDynamic: [
+    '/node_modules/scheduler/cjs/scheduler.production.min.js',
+    '/node_modules/function-bind/**',
+    '/node_modules/@clerk/shared/dist/chunk-RSOCGYTF.mjs'
+  ],
 };
  
