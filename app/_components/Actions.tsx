afc8a50236c0543beadbbc635ab06746287f4648
Sven <iamsven2005@gmail.com>
2024-01-08 23:54:12 +0800

    gg

--- app/_components/Actions.tsx
+++ app/_components/Actions.tsx
@@ -1,9 +1,12 @@
-import {currentUser} from "@clerk/nextjs";
+import {SignInButton, currentUser} from "@clerk/nextjs";
 
 export const Actions = async () => {
     const user = await currentUser();
     return (
         <div className="form-control gap-2">
+            {!user && (
+                <SignInButton className="btn btn-primary"/>
+            )}
         {user && (
         <a href={`/u/${user.username}`} className="btn">
             Dashboard
