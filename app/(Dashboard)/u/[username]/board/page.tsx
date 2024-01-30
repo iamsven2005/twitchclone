a5ee4816eb6acbe7845d3c1869ad8d325c300cf0
Student <iamsven2005@gmail.com>
2024-01-30 18:59:21 +0800

    gg

--- app/(Dashboard)/u/[username]/board/page.tsx
+++ app/(Dashboard)/u/[username]/board/page.tsx
@@ -1,21 +1,28 @@
 "use client"
 import { CreateOrganization } from "@clerk/nextjs";
 import { List } from "./_components/list";
-const BoardPage = () => {
+import React from "react";
+
+const BoardPage: React.FC = () => {
+    const openModal = () => {
+        const dialogElement = document.getElementById('new_board') as HTMLDialogElement | null;
+        if (dialogElement) {
+            dialogElement.showModal();
+        }
+    };
 
     return (
         <div>
-        <dialog id="new_board" className="modal">
-            <CreateOrganization />
-            <form method="dialog" className="modal-backdrop">
-                <button>close</button>
-            </form>
-        </dialog>
-        <button className="btn" onClick={()=>document.getElementById('new_board').showModal()}>open modal</button>
+            <button className="btn" onClick={openModal}>Open Modal</button>
+            <dialog id="new_board" className="modal">
+                <CreateOrganization />
+                <form method="dialog">
+                    <button className="btn" onClick={() => document.getElementById('new_board')?.close()}>Close</button>
+                </form>
+            </dialog>
 
-        <List/>
+            <List />
         </div>
-   
     );
 }
 
