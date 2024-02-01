08dd700239163b233d1558ec1167bdcd771a6e8a
Student <iamsven2005@gmail.com>
2024-02-01 18:43:47 +0800

    new

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -1,7 +1,7 @@
 import { auth, currentUser } from "@clerk/nextjs";
 import { Liveblocks } from "@liveblocks/node";
 import { ConvexHttpClient } from "convex/browser";
-import { api } from "@/convex/_generated/api";
+import { api } from "@/convex/_generated/api"; 
 
 const convex = new ConvexHttpClient(
   process.env.NEXT_PUBLIC_CONVEX_URL!
